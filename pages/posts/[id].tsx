0213e5e2356fca1192c2ca4a49424b1d76ff08e4
leegyuseung <lgs4002@naver.com>
2022-08-22 23:53:28 +0900

    스타일링

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,6 +2,7 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 import React from "react";
 import { getAllPostIds, getPostData } from "../../lib/post";
+import postStyles from "../../styles/Post.module.css";
 
 const Post = ({
   postData,
@@ -13,7 +14,7 @@ const Post = ({
   };
 }) => {
   return (
-    <div>
+    <div className={postStyles.container}>
       <Head>
         <title>{postData.title}</title>
       </Head>
